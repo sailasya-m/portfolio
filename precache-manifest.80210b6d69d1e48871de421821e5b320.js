3c3e1fe9f282a526a28e345cf530f3557636c322
sailasya-m <mynampatisailasya@gmail.com>
2024-04-20 23:42:31 +0530

    Updates

---
+++ precache-manifest.80210b6d69d1e48871de421821e5b320.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7d690895a57df9eed1d91e686bac5f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0bca7d1c0dfc901ce995",
+    "url": "/portfolio/static/css/2.10a3c586.chunk.css"
+  },
+  {
+    "revision": "7e43062d1d85e179ab81",
+    "url": "/portfolio/static/css/main.71368948.chunk.css"
+  },
+  {
+    "revision": "0bca7d1c0dfc901ce995",
+    "url": "/portfolio/static/js/2.13d14889.chunk.js"
+  },
+  {
+    "revision": "141eb1e228498ca0a44a",
+    "url": "/portfolio/static/js/3.9cdb8ae0.chunk.js"
+  },
+  {
+    "revision": "7e43062d1d85e179ab81",
+    "url": "/portfolio/static/js/main.869a2d6e.chunk.js"
+  },
+  {
+    "revision": "3fd6443f30083b174e02",
+    "url": "/portfolio/static/js/runtime~main.9691e06e.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/portfolio/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/portfolio/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "43979650511112be9d75a69aa260bae9",
+    "url": "/portfolio/static/media/bakery_1_1000x600.43979650.jpg"
+  },
+  {
+    "revision": "f193711c72ad8402020228d07bdd7570",
+    "url": "/portfolio/static/media/clothing_2_1000x600.f193711c.jpg"
+  },
+  {
+    "revision": "af51fca00c9ec0a42f8a09e1bca102d3",
+    "url": "/portfolio/static/media/coffe1_2_1000x600.af51fca0.jpg"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/portfolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "087a6d45e77185ce6ed47495fee87ec8",
+    "url": "/portfolio/static/media/flow.087a6d45.png"
+  },
+  {
+    "revision": "47dc9fdce9e969cb36cf6c162fed02fc",
+    "url": "/portfolio/static/media/flower-unscreen.47dc9fdc.gif"
+  },
+  {
+    "revision": "c61cd5a1a9dc2b943d5abf91267c968d",
+    "url": "/portfolio/static/media/girl lap-Photoroom.png-Photoroom.c61cd5a1.png"
+  },
+  {
+    "revision": "b4bbe44f2919da02d3d975bf5af3a5be",
+    "url": "/portfolio/static/media/girl_lap.b4bbe44f.png"
+  },
+  {
+    "revision": "30b575b6871606e5da9df586f80d4eb7",
+    "url": "/portfolio/static/media/lilac bg.30b575b6.jpeg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "d08c192991a64f9aa83f78e02b5a3c28",
+    "url": "/portfolio/static/media/nav-icon2.d08c1929.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/portfolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "862e63f1d24b04992e4dd36e4aa119b2",
+    "url": "/portfolio/static/media/petshop_2_1000x600.862e63f1.jpg"
+  },
+  {
+    "revision": "068a259bd71da59c7da9c795585a11e7",
+    "url": "/portfolio/static/media/restaurant_2_1000x600.068a259b.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "e93e5b3a9d2943314f2845265aee3bfc",
+    "url": "/portfolio/static/media/travel-web_2_1000x600.e93e5b3a.jpg"
+  }
+]);
